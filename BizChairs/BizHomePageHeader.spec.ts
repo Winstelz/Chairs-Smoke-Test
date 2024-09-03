fc0bc4203ef32b8643ce48cbeffed9062f878049
Winstelz <Winstelz@gmail.com>
2024-09-03 15:39:27 -0400

    Add files via upload

--- BizChairs/BizHomePageHeader.spec.ts
+++ BizChairs/BizHomePageHeader.spec.ts
@@ -7,7 +7,7 @@ test('BizHomePageHeader', async ({ page }) => {
     const HomePage = new BizChairHomePage(page)
 
 //Navigate to BizChair site    
-   HomePage.gotoHomePage();
+    await HomePage.gotoHomePage();
 //Click Logo and verify on homepage
     await HomePage.ClickLogo();
 //Click Office Menu
@@ -15,20 +15,11 @@ test('BizHomePageHeader', async ({ page }) => {
 //Click Folding Menu
     await HomePage.ClickFolding();
 //Click Event Menu
-    const Event = page.locator("//span[@title='Event']//a[normalize-space()='Event']");
-    await Event.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/event');
+    await HomePage.ClickEvent();
 //Click Restaurant Menu
-    const Restaurant = page.locator("//span[@title='Restaurant']//a[normalize-space()='Restaurant']");
-    await Restaurant.click();
-    await page.waitForTimeout(2000);
-    expect(page.url()).toContain('/collections/restaurant');
+    await HomePage.ClickRestaurant();
 //Click Church Menu
-    const Church = page.locator("//span[@title='Church']//a[normalize-space()='Church']");
-    await Church.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/church');
+    await HomePage.ClickChurch();
 //Click Classroom Menu
     const Classroom = page.locator("//span[@title='Classroom']//a[normalize-space()='Classroom']");
     await Classroom.click();
@@ -46,23 +37,11 @@ test('BizHomePageHeader', async ({ page }) => {
 //Hover Folding Menu & Click Resin Folding Chairs
     await HomePage.HoverFolding();
 //Hover Event Menu & Click Folding Chairs
-    await Event.hover();
-    const FoldingChairs = page.locator("//ul[@aria-labelledby='event-menu']//a[@title='Folding Chairs'][normalize-space()='Folding Chairs']");
-    await FoldingChairs.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/folding-chairs');
+    await HomePage.HoverEvent();
 //Hover Restaurant Menu & Click Indoor Dining Chairs
-    await Restaurant.hover();
-    const IndoorDining = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Indoor Dining Chairs']");
-    await IndoorDining.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/indoor-restaurant-dining-chairs');
+    await HomePage.HoverRestaurant();
 //Hover Church Menu & Click Banquet Stack Chairs
-    await Church.hover();
-    const Banquet = page.locator("//ul[@aria-labelledby='church-menu']//a[@title='Banquet Stack Chairs'][normalize-space()='Banquet Stack Chairs']");
-    await Banquet.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/banquet-stack-chairs');
+    await HomePage.HoverChurch();
 //Hover Classroom Menu & Click Student Desk
     await Classroom.hover();
     const StudentDesks = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Student Desks']");
