b5cc1b134d45635c18960e7f4cd88e9b96d6ae4a
Winstelz <Winstelz@gmail.com>
2024-09-04 08:48:48 -0400

    Add files via upload

--- Advantage Chairs/AdvantagePLP.spec.ts
+++ Advantage Chairs/AdvantagePLP.spec.ts
@@ -1,66 +1,34 @@
 import { test, expect } from '@playwright/test';
+import { AdvantageHomePage }from '../../pages/AdvantageHomePage';
+import { AdvantagePLP }from '../../pages/AdvantagePLP';
 
 
 
 test('AdvantagePLP', async ({ page }) => {
-    //Navigate to Advantage site    
-        await page.goto('https://www.advantagechurchchairs.com/?_ab=0&_fd=0&_sc=1&preview_theme_id=164106633506');
+
+    const HomePage = new AdvantageHomePage(page)
+    const PLP = new AdvantagePLP(page)
+//Navigate to Advantage site   
+    await HomePage.gotoHomePage() 
 //Click Church Chairs Menu
-    const ChurchChairs = page.locator("//span[@title='Church Chairs']");
-    await ChurchChairs.click(); 
-    expect(page.url()).toContain('/collections/church-stack-chairs');
+    await HomePage.ClickChurchChairs();
 //Click Sort button Price low to high
-    const Sort = page.locator("//select[@id='SortBy']");
-    await Sort.inputValue("Price, low to high");
-    const FirstItem = page.locator("//a[contains(text(),'Advantage Multipurpose Church Chairs - 18.5 in. Wi')]");
-    expect(FirstItem).toContainText("Advantage Multipurpose Church Chairs");
+    await PLP.Sorting();
 //Click Color Family -> Green
-    const ColorFam = await page.locator("//button[normalize-space()='Color Family']");
-    await ColorFam.isVisible();
-    await ColorFam.click();
-    const ShowMore = await page.locator("//div[@data-toggle-filter='color-family']");
-    await ShowMore.isVisible();
-    await ShowMore.click();
-    const Green = await page.locator("//label[contains(@for,'filter-p-m-filter-colors_green')]//div[contains(@class,'ra-choice__checkmark set--inherit-focus')]");
-    await Green.isVisible();
-    await Green.click();
-    await page.waitForTimeout(2000);
+    await PLP.ColorFilter();
 //Click Finish -> Copper Vein Metal
-    const Finish = await page.locator("//button[normalize-space()='Finish']");
-    await Finish.isVisible();
-    await Finish.click();
-    const Copper = await page.locator("//span[contains(@class,'ra-choice__label')][normalize-space()='Copper Vein Metal']");
-    await Copper.isVisible();
-    await Copper.click();
+    await PLP.FinishFilter();
 //Clear Filter Pills
-    const GreenPill = await page.locator("//button[contains(@data-param,'filter.p.m.filter.colors')]//span[contains(@class,'ra-icon')]//*[name()='svg']");
-    await GreenPill.isVisible();
-    await GreenPill.click();
-    const ClearAll = await page.locator("//button[normalize-space()='Clear All']");
-    await ClearAll.isVisible();
-    await ClearAll.click();
+    await PLP.ClearColorFilter();
+    await PLP.ClearAllFilter();
 //Click Pagination
-    const Page2 = await page.locator("//a[normalize-space()='2']");
-    await Page2.isVisible();
-    await Page2.click();
-    expect(page.url()).toContain("page=2");
+   await PLP.ClickPagination();
 //Click Pagination Right Arrow
-    const RightArrow = await page.locator("//a[@aria-label='Go to next page']");
-    await RightArrow.isVisible();
-    await RightArrow.click();
-    expect(page.url()).toContain("page=3");
+    await PLP.ClickRightArrow()
 //Click Pagination Left Arrow
-    const LeftArrow = await page.locator("//a[@aria-label='Go to previous page']");
-    await LeftArrow.isVisible();
-    await LeftArrow.click();
-    expect(page.url()).toContain("page=2");
+    await PLP.ClickLeftArrow();
 //Click on PDP
-    await page.waitForLoadState();
-    const PDPItem = await page.locator("//img[@alt=\"Embroidered 21''W Church Chair in Empire Fabric with Book Rack - Gold Vein Frame - View 2\"]");
-    await PDPItem.isVisible();
-    await PDPItem.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain("21w-church-chair-in-galaxy-fabric");
+    await PLP.ClickPDP();
 
     
 
