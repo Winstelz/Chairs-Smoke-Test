f286f0fa985fdd9315723dcbdd6d6f961c20ed50
Winstelz <Winstelz@gmail.com>
2024-09-04 08:24:19 -0400

    Add files via upload

--- Advantage Chairs/AdvantageHomePageHeader.spec.ts
+++ Advantage Chairs/AdvantageHomePageHeader.spec.ts
@@ -1,76 +1,39 @@
 import { test, expect } from '@playwright/test';
+import { AdvantageHomePage }from '../../pages/AdvantageHomePage';
 
 
 
 test('AdvantageHomePageHeader', async ({ page }) => {
-    //Navigate to Advantage site    
-        await page.goto('https://www.advantagechurchchairs.com/?_ab=0&_fd=0&_sc=1&preview_theme_id=164106633506');
+
+    const HomePage = new AdvantageHomePage(page)
+//Navigate to Advantage site  
+    await HomePage.gotoHomePage()
 
 //Click Logo and verify on homepage
-    const Logo = await page.locator("//img[@alt='Advantage Church Chairs Logo']");
-    await Logo.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('https://www.advantagechurchchairs.com/');
+    await HomePage.ClickLogo();
 //Click Shop All Menu
-    const ShopAll = page.locator("//span[@title='Shop All']//a[normalize-space()='Shop All']");
-    await ShopAll.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/shop-all');
+    await HomePage.ClickShopAll();
 //Click Church Chairs Menu
-    const ChurchChairs = page.locator("//span[@title='Church Chairs']");
-    await ChurchChairs.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/church-stack-chairs');
+    await HomePage.ClickChurchChairs();
 //Click Banquet Chairs Menu
-    const BanquetChairs = page.locator("//span[@title='Banquet Chairs']");
-    await BanquetChairs.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/banquet-stack-chairs');
+    await HomePage.ClickBanquetChairs();
 //Click Folding & Event Menu
-    const FoldEvent = page.locator("//span[@title='Folding & Event']//a[normalize-space()='Folding & Event']");
-    await FoldEvent.click();
-    await page.waitForTimeout(2000);
-    expect(page.url()).toContain('/collections/folding-event');
+    await HomePage.ClickFoldEvent();
 //Click Classroom Menu
-    const Classroom = page.locator("//span[@title='Classroom']//a[normalize-space()='Classroom']");
-    await Classroom.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/classroom');
+    await HomePage.ClickClassroom();
 //Click Office & Reception Menu
-    const Office = page.locator("//span[@title='Office & Reception']//a[normalize-space()='Office & Reception']");
-    await Office.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/office');
+    await HomePage.ClickOffice();
+
+
 //Hover Church Chairs Menu & Click Church & Stack Chairs Dollies
-    await ChurchChairs.hover();
-    const Dollies = page.locator("//a[@class='text-base leading-4 tracking-wider flex flex-col'][normalize-space()='Church & Stack Chair Dollies']");
-    await Dollies.click();
-    await page.waitForLoadState('load', { timeout: 60000 });
-    expect(page.url()).toContain('/collections/church-banquet-stack-chair-dollies');
+    await HomePage.HoverChurchChairs();
 //Hover Folding & Event Menu & Click Resin Folding Chairs
-    await FoldEvent.hover();
-    const Resin = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Resin Folding Chairs']");
-    await Resin.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/resin-folding-chairs');
+    await HomePage.HoverFoldEvent();
 //Hover Classroom Menu & Click Activity Set
-    await Classroom.hover();
-    const Activity = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Activity Sets']");
-    await Activity.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/classroom-activity-table-sets');
+    await HomePage.HoverClassroom();
 //Hover Office Menu & Click Desks
-    await Office.isVisible();
-    await Office.hover();
-    const Desks = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Desks']");
-    await Desks.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/desks');
+    await HomePage.HoverOffice();
 //Hover More Menu & Click Patio & Outdoor
-    const More = page.locator("//span[@title='More']");
-    await More.hover();
-    const Patio = page.locator("//a[@class='text-base leading-4 tracking-wider flex flex-col' and normalize-space()='Patio & Outdoor']");
-    await Patio.click();
-    await page.waitForLoadState();
-    expect(page.url()).toContain('/collections/patio-outdoor');
+    await HomePage.HoverMore();
+    
 });
