2c0d27d92648b1fcdba1cfcef5a3d66f073f474a
Winstelz <Winstelz@gmail.com>
2024-09-16 08:21:56 -0400

    Add files via upload

--- Advantage Chairs/AdvantageAnnoucementBar.spec.ts
+++ Advantage Chairs/AdvantageAnnoucementBar.spec.ts
@@ -1,23 +1,17 @@
 import { test, expect } from '@playwright/test';
+import { AdvantageHomePage }from '../../pages/AdvantageHomePage';
+import { AdvantageAnnoucementBar }from '../../pages/AdvantageAnnoucementBar';
 
 
 
 test('AnnoucementBar', async ({ page }) => {
-    //Navigate to Advantage site    
-        await page.goto('https://www.advantagechurchchairs.com/?_ab=0&_fd=0&_sc=1&preview_theme_id=164106633506');
+    const HomePage = new AdvantageHomePage(page)
+    const Banner = new AdvantageAnnoucementBar(page)
+ //Navigate to Advantage site    
+    await HomePage.gotoHomePage();
 //Click through the Bar
-    const RightArrow = await page.locator("//div[@aria-label='Next slide']//span[1]");
-    const LeftArrow = await page.locator("//div[@aria-label='Previous slide']//span[1]");
-    await RightArrow.click();
-    await page.waitForTimeout(2000);
-    await RightArrow.click();
-    await page.waitForTimeout(2000);
-    await LeftArrow.click();
-    await page.waitForTimeout(2000);
-    await LeftArrow.click();
-    await page.waitForTimeout(2000);
-    await RightArrow.click();
-    await page.waitForTimeout(2000);
+    await Banner.ClickBanner();
+    
 
 
 
