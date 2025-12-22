92da60e4d87081e52832f37462c976838919f42d
Winstelz <Winstelz@gmail.com>
2025-12-22 15:30:30 -0500

    Add files via upload

---
+++ tests/bestChiavariChairs/BestChiavariHomePageHeader.spec.ts
@@ -0,0 +1,76 @@
+import { test, expect } from '@playwright/test';
+
+
+
+test('BestChiavariHomePageHeader', async ({ page }) => {
+    //Navigate to Best Chiavari site    
+        await page.goto('https://www.bestchiavarichairs.com/?_ab=0&_fd=0&_sc=1&preview_theme_id=161761132832');
+
+//Click Logo and verify on homepage
+    const Logo = await page.locator("//img[@alt='Best Chiavari Chairs Logo']");
+    await Logo.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('https://www.bestchiavarichairs.com/');
+//Click Chiavari Chairs Menu
+    const Chiavari = page.locator("//span[@title='Chiavari Chairs']//a[normalize-space()='Chiavari Chairs']");
+    await Chiavari.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/chiavari-chairs');
+//Click Tables Menu
+    const Tables = page.locator("//span[@title='Tables']//a[normalize-space()='Tables']");
+    await Tables.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/banquet-cocktail-and-dining-tables');
+//Click Folding Chairs Menu
+    const FoldingChairs = page.locator("//span[@title='Folding Chairs']//a[normalize-space()='Folding Chairs']");
+    await FoldingChairs.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/folding-chairs');
+//Click Cross Back Chairs Menu
+    const CrossBack = page.locator("//span[@title='Cross Back Chairs']//a[normalize-space()='Cross Back Chairs']");
+    await CrossBack.click();
+    await page.waitForTimeout(2000);
+    expect(page.url()).toContain('/collections/cross-back-dining-event-chairs');
+//Click Banquet Chairs Menu
+    const BanquetChairs = page.locator("//span[@title='Banquet Chairs']//a[normalize-space()='Banquet Chairs']");
+    await BanquetChairs.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/banquet-stack-chairs');
+//Click Ghost Chairs Menu
+    const GhostChairs = page.locator("//span[@title='Ghost Chairs']//a[normalize-space()='Ghost Chairs']");
+    await GhostChairs.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/ghost-chairs-stools');
+//Click Dollies & Carts
+    const Dollies = page.locator("//span[@title='Dollies & Carts']//a[normalize-space()='Dollies & Carts']");
+    await Dollies.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/dollies-carts');
+
+//Hover Chiavari Chairs Menu & Click Wood Chiavari Chairs
+    await Chiavari.hover();
+    const WoodChiavari = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Wood Chiavari Chairs']");
+    await WoodChiavari.click();
+    await page.waitForLoadState('load', { timeout: 60000 });
+    expect(page.url()).toContain('/collections/wood-chiavari-chairs');
+//Hover Tables Menu & Click Wood Folding Tables
+    await Tables.hover();
+    const WoodFolding = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Wood Folding Tables']");
+    await WoodFolding.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/wood-folding-tables');
+//Hover Folding Chairs Menu & Click Resin Folding Chairs
+    await FoldingChairs.hover();
+    const Resin = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Resin Folding Chairs']");
+    await Resin.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/resin-folding-chairs');
+//Hover More For Your Venue Menu & Click
+    const More = page.locator("//span[@title='More For Your Venue']//a[@href='#'][normalize-space()='More For Your Venue']");
+    await More.hover();
+    const PatioSets = page.locator("//a[@class='flex flex-col font-normal text-e14 leading-e150'][normalize-space()='Commercial Patio Seating']");
+    await PatioSets.click();
+    await page.waitForLoadState();
+    expect(page.url()).toContain('/collections/commercial-patio-seating');
+
+});
---
+++ tests/bestChiavariChairs/BestChiavariInlineCart.spec.ts
@@ -0,0 +1,84 @@
+import { test, expect } from '@playwright/test';
+
+
+
+
+test('BestChiavariInlineCart', async ({ page }) => {
+    //Navigate to Best Chiavari site    
+        await page.goto('https://www.bestchiavarichairs.com/?_ab=0&_fd=0&_sc=1&preview_theme_id=161761132832');
+    //Navigate to PLP
+        const ShopAll = page.locator("//span[@title='Banquet Chairs']//a[normalize-space()='Banquet Chairs']");
+        await ShopAll.click();
+        expect(page.url()).toContain("banquet-stack-chairs");
+    //Click First Item
+        const FirstItem = page.locator("//img[@alt='HERCULES Series Crown Back Stacking Banquet Chair - View 2']");
+        await FirstItem.click();
+        expect(page.url()).toContain("crown-back-stacking-banquet-chair");;
+    //Add Item to Cart
+        const AddtoCart = page.locator("//button[normalize-space()='Add to Cart']");
+        await AddtoCart.click();
+        await page.waitForTimeout(2000);
+    //Verify Product is in Cart
+        const Product = await page.locator("//a[normalize-space()='HERCULES Series Crown Back Stacking Banquet Chair']");
+        await Product.isVisible();
+        expect(Product).toHaveText("HERCULES Series Crown Back Stacking Banquet Chair");
+    //Increase QTY
+        const QTYIncr = page.locator("(//button[@aria-label='Increment Quantity'])[2]");
+        await QTYIncr.click();
+        await page.waitForTimeout(1000);
+        await QTYIncr.click();
+        await page.waitForTimeout(1000);
+        await QTYIncr.click();
+        await page.waitForTimeout(1000);
+        await QTYIncr.click();
+        await page.waitForTimeout(2000);
+        const QTY = page.locator("(//input[@type='number'])[2]");
+        await expect(QTY).toHaveValue("5");
+    //Decrease QTY
+        const QTYDecr = page.locator("(//button[@aria-label='Decrement Quantity'])[2]");
+        await QTYDecr.click();
+        await page.waitForTimeout(1000);
+        await QTYDecr.click();
+        await page.waitForTimeout(2000);
+        await expect(QTY).toHaveValue("3");
+    //Input QTY
+        await QTY.click();
+        await QTY.fill("10");
+        await page.waitForTimeout(2000);
+        await expect(QTY).toHaveValue("10");
+    //You May Also Like Carousel Clicking
+        const RightArrow = page.locator("//button[@aria-label='Next slide']//span[@class='ra-icon ra-icon--sm']//*[name()='svg']");
+        await RightArrow.click();
+        await RightArrow.click();
+        const LeftArrow = page.locator("//button[@aria-label='Previous slide']//span[@class='ra-icon ra-icon--sm']//*[name()='svg']");
+        await LeftArrow.click();
+        await LeftArrow.click();
+    //Calculate Shipping
+        await page.locator("//span[@class='ra-icon']//*[name()='svg']").click();
+        const Zip = page.locator("//input[@placeholder='Enter Zip or Postal Code']");
+        await Zip.click();
+        await Zip.fill("45014");
+        const Address = page.locator("//select[@name='addressType']");
+        await Address.click();
+        await Address.selectOption('Residential');
+        await page.locator("//button[normalize-space()='Calculate Shipping']").click();
+        await page.waitForTimeout(2000);
+    //Click Checkout
+        await page.locator("//button[normalize-space()='Proceed to Checkout']").click();
+        await page.locator("//header[@class='EAjaz Xx7bI _1fragemr6']//div//div//a[@class='s2kwpi1 s2kwpi0 _1fragempf _1fragemwu _1fragemx3 _1fragemwp s2kwpi3 s2kwpi7 s2kwpi5 _1fragemwl']").click();
+        await page.waitForLoadState();
+        await page.locator("(//a[@title='cart'])[1]").click();
+    //Delete Item from Inline Cart
+        await page.getByRole('button', { name: 'Remove item' }).click();
+        const EmptyLink = await page.locator("//a[normalize-space()='Shop Chiavari Chairs']");
+        await EmptyLink.click();
+        await page.waitForLoadState();
+        expect(page.url()).toContain("/collections/chiavari-chairs");
+
+
+
+
+        
+
+
+});
