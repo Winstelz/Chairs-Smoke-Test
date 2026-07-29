458c6dbf2a0d2322b2d6a2d40ab1b0059f87d263
mwinstel-gworks <Winstelz@gmail.com>
2026-07-29 08:17:21 -0400

    Chiavari Announcement Bar Updates

--- tests/bestChiavariChairs/bestChiavariAnnoucementBar.spec.ts
+++ tests/bestChiavariChairs/bestChiavariAnnoucementBar.spec.ts
@@ -1,14 +1,19 @@
 import { test, expect } from '@playwright/test';
-import { AdvantageHomePage } from '../../src/pom/advantageChairs/advantageHomePage';
+import { BestChiavairHomePage } from '../../src/pom/bestChiavairChairs/bestChiavairHomePage';
 import { CommonHomePage } from '../../src/pom/commonHomePage';
 
 
 
 test('AnnoucementBar Flow', async ({ page }) => {
-    const homePage = new AdvantageHomePage(page)
+    const homePage = new BestChiavairHomePage(page);
     const commonHomePage = new CommonHomePage(page);
- //Navigate to Advantage site    
+
+//Navigate to Best Chiavari site    
     await homePage.gotoHomePage();
 //Click through the Bar
     await commonHomePage.clickBanner(homePage.bannerContainer);
+
+
+
+
 });
